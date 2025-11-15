911df73a13dce79673066c3fc6527a963e15285a
martianbytes <195779386+martianbytes@users.noreply.github.com>
2025-11-15 09:18:19 +0545

    the ui

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+// get all the dom elements that are necessary
+const display = document.querySelector('#display');
+const keypad = document.querySelector("#keypad");
+const clearButton = document.querySelector('[data-action="clear"]');
+
